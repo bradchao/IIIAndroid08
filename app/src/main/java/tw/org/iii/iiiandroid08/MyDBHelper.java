b297e9cd7ca9626595d93debe38fb22f9bc30c9e
brad <brad@brad.tw>
2019-11-15 16:50:13 +0800

    6

---
+++ app/src/main/java/tw/org/iii/iiiandroid08/MyDBHelper.java
@@ -0,0 +1,27 @@
+package tw.org.iii.iiiandroid08;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private static final String createTable =
+            "CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                    ", username TEXT, tel TEXT, birthday DATE)";
+
+    public MyDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
