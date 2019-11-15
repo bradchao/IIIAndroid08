99f332983ba2734d6a156ce657251d7e3571fcc5
brad <brad@brad.tw>
2019-11-15 14:36:31 +0800

    2

--- app/src/main/java/tw/org/iii/iiiandroid08/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid08/MainActivity.java
@@ -2,13 +2,72 @@ package tw.org.iii.iiiandroid08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
+    private TextView content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        content = findViewById(R.id.content);
+        sp = getSharedPreferences("brad", MODE_PRIVATE);
+        editor = sp.edit();
+    }
+
+    public void test1(View view) {
+        editor.putString("username", "brad");
+        editor.putBoolean("sound", false);
+        editor.putInt("stage", 4);
+        editor.commit();
+        Toast.makeText(this, "save ok", Toast.LENGTH_SHORT).show();
+    }
+
+    public void test2(View view) {
+        boolean isSound = sp.getBoolean("sound", true);
+        String username = sp.getString("username", "nobody");
+        int stage = sp.getInt("stage", 1);
+        Log.v("brad", username + ":" + stage + ":" + isSound);
+    }
+
+    public void test3(View view) {
+        try {
+            FileOutputStream fout = openFileOutput("brad.txt", MODE_APPEND);
+            fout.write("Hello, World\n".getBytes());
+            fout.flush();
+            fout.close();
+            Toast.makeText(this, "Save OK", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.v("brad", e.toString());
+        }
+    }
+
+    public void test4(View view) {
+        try (FileInputStream fin = openFileInput("brad.txt")){
+            StringBuffer sb = new StringBuffer();
+            int c;
+            while ((c = fin.read()) != -1){
+                sb.append((char)c);
+            }
+            content.setText(sb.toString());
+        }catch (Exception e){
+            Log.v("brad", e.toString());
+        }
+
+
     }
 }
